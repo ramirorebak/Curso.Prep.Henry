189e8e69fa9d5f373b092d3b73dc1cb2cddbd01b
ramirorebak <96314779+ramirorebak@users.noreply.github.com>
2022-02-01 10:24:33 -0300

    Avance, 02-JS-l

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -115,21 +115,29 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  if (num % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
-  
+  return Math.pow(num, 2);
 }
 
 function elevarAlCubo(num) {
