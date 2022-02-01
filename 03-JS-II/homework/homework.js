fd83acc9a5022639918aefa830ef1de7e7dc4fdb
ramirorebak <96314779+ramirorebak@users.noreply.github.com>
2022-02-01 19:30:57 -0300

    Avances, 03-JS-II

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -118,6 +118,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
+  if (numero % 1 === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function fizzBuzz(numero) {
@@ -125,6 +130,15 @@ function fizzBuzz(numero) {
   // Si "numero" es divisible entre 5, devuelve "buzz"
   // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
   // De lo contrario, devuelve el numero
+if (numero % 3 === 0 && numero % 5 === 0) {
+  console.log ('fizzbuzz');
+ } else if (numero % 3 === 0) {
+  console.log ('fizz');
+ } else if (numero % 5 === 0) {
+  console.log ('buzz');
+ } else {
+  console.log (numero);
+ }
 }
 
 function operadoresLogicos(num1, num2, num3) {
@@ -134,6 +148,17 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return 'Error';
+  } else if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return 'Hay negativos';
+  } else if (num1 > num2 && num1 > num3 && num1 > 0) {
+    return 'Numero 1 es mayor y positivo';
+  } else if (num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  } else {
+    return false;
+  }
 }
 
 function esPrimo(numero) {
