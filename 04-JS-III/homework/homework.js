0e6ae19c4a57b2b8c24336f62fc8278efba34000
ramirorebak <96314779+ramirorebak@users.noreply.github.com>
2022-02-07 19:34:11 -0300

    04-JS-III

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -158,6 +158,12 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
+  var nueve = n.toString();
+  if (nueve[0] === '9') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
@@ -165,17 +171,29 @@ function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
   //Escribe tu código aquí  
-  for (let i = 0; i < arreglo.length; i++) {
-    if (arreglo[i] )
+  for (var i = 0; i < arreglo.length; i++) {
+    if (arreglo[i] !== arreglo[i + 1]) {
+      return false;
+    } else {
+      return true;
+    }
   }
 } 
-
 
 function mesesDelAño(array) {
   //Dado un array que contiene algunos meses del año desordenados, recorrer el array buscando los meses de 
   // "Enero", "Marzo" y "Noviembre", guardarlo en nuevo array y retornarlo.
   //Si alguno de los meses no está, devolver: "No se encontraron los meses pedidos"
   // Tu código:
+  var nuevo = []
+   for (var i = 0; i < array.length; i++) {
+    if (array[i] === 'Enero' || array[i] === 'Marzo' || array[i] === 'Noviembre') {
+      nuevo.push(array[i]);
+    }
+  }
+  if (nuevo.length !== 3) {
+    return 'No se encontraron los meses pedidos';
+  } else return nuevo;
 }
 
 
@@ -183,8 +201,14 @@ function mayorACien(array) {
   //La función recibe un array con enteros entre 0 y 200. Recorrer el array y guardar en un nuevo array sólo los
   //valores mayores a 100 (no incluye el 100). Finalmente devolver el nuevo array.
   // Tu código:
+  var arrai = []
+  for (var i = 0; i <= array.length; i++) {
+    if (array[i] > 100) {
+      arrai.push(array[i]);
+    }
+  }
+    return arrai;
 }
-
 
 function breakStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
@@ -194,8 +218,22 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var aumento = numero;
+  var array = [];
+  for (var i = 0; i < 10; i++) {
+    aumento = aumento + 2;
+   if (aumento === i) {
+    break;
+  } else {
+    array.push(aumento)
+  }
 }
-
+  if (i < 10) {
+    return 'Se interrumpió la ejecución';
+  } else {
+    return array;
+  }
+}
 
 function continueStatement(numero) {
   //Iterar en un bucle aumentando en 2 el numero recibido hasta un límite de 10 veces.
@@ -204,6 +242,22 @@ function continueStatement(numero) {
   //Cuando el número de iteraciones alcance el valor 5, no se suma en ese caso y se continua con la siguiente iteración
   //Pista: usá el statement 'continue'
   // Tu código:
+    var aumento = numero;
+  var array = [];
+  for (var i = 0; i < 10; i++) {
+    aumento = aumento + 2;
+   if (i === 5) {
+    continue;
+  } else {
+    array.push(aumento);
+  }
+}
+  if (i < 10) {
+    return i;
+  } else {
+    return array;
+  }
+
 }
 
 
